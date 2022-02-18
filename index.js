a0402b930902fea4286deb6ba878d6e4a8e1132a
Craig Ford <craig_ford@icloud.com>
2022-02-17 19:41:32 -0500

    commit tree b8282b75fe4be391596d52555cbfac6c00c2b32f

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// Write your code here!
+
 main.remove();
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.textContent = "'Craig Ford' is the champion";
+newHeader.innerHTML = "'Craig Ford' is the champion";
+
+document.body.append(newHeader);
